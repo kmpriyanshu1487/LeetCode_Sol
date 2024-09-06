f4dca4812e7734fc154ba452c9f097c8415ee89d
kmpriyanshu/webdev <149373747+kmpriyanshu1487@users.noreply.github.com>
2024-09-06 19:24:48 +0530

    Time: 4 ms (41.17%), Space: 14 MB (43.50%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -18,7 +18,7 @@ public:
                 } else {
                     left = mid + 1; 
                 }
-            } else { \
+            } else { 
                 if (target >= nums[mid] && target <= nums[right]) {
                     left = mid + 1; 
                 } else {
